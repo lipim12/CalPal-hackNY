d601f87bb95eb5e11ac681080a64b16301cfc8d3
Vrajesh Mehta <vmehta342@gmail.com>
2019-09-29 01:46:19 -0400

    small update

--- db_tasks/db_utils.py
+++ db_tasks/db_utils.py
@@ -3,7 +3,7 @@ import os
 
 def connect_db():
     username = os.environ.get("db_user")
-    password = os("db_pass")
+    password = os.environ.get("db_pass")
     client = MongoClient("mongodb+srv://"+username+":"+password+"@cluster0-750e4.gcp.mongodb.net/test?retryWrites=true&w=majority")
     db = client.test
     return db
